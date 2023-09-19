ce30d7642f891b56034dd2f6052a38d6b313904f
Sai Chaithanya <saichaithanya314@gmail.com>
2023-09-19 21:44:22 +0530

    fixed shfmt linting issue

--- trigger.sh
+++ trigger.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 bin_dir=$(pwd)/tests/ffmpeg_bin
 rm -rf "$bin_dir"
-mkdir -p "$bin_dir" 
-if [[ "$(docker images -q saichaithanya/ffmpeg-build-toolchain:v1 2> /dev/null)" == "" ]]; then
-    if [[ "$(docker manifest inspect saichaithanya/ffmpeg-build-toolchain:v1 2> /dev/null)" == "" ]]; then
-        docker build -t saichaithanya/ffmpeg-build-toolchain:v1 ./build-tools &&
-        docker image push saichaithanya/ffmpeg-build-toolchain:v1
-    else
-        docker pull saichaithanya/ffmpeg-build-toolchain:v1
-    fi
+mkdir -p "$bin_dir"
+if [[ "$(docker images -q saichaithanya/ffmpeg-build-toolchain:v1 2>/dev/null)" == "" ]]; then
+	if [[ "$(docker manifest inspect saichaithanya/ffmpeg-build-toolchain:v1 2>/dev/null)" == "" ]]; then
+		docker build -t saichaithanya/ffmpeg-build-toolchain:v1 ./build-tools &&
+			docker image push saichaithanya/ffmpeg-build-toolchain:v1
+	else
+		docker pull saichaithanya/ffmpeg-build-toolchain:v1
+	fi
 fi
-if [[ "$(docker images -q ffmpeg-buildmaster:latest 2> /dev/null)" == "" ]]; then
-    docker build -t ffmpeg-buildmaster ./buildmaster
+if [[ "$(docker images -q ffmpeg-buildmaster:latest 2>/dev/null)" == "" ]]; then
+	docker build -t ffmpeg-buildmaster ./buildmaster
 fi
 docker run -d -t --name=ffmpeg-buildmaster ffmpeg-buildmaster &&
-docker cp ffmpeg-buildmaster:/root/ffmpeg_bin/. "$bin_dir" &&
-docker stop ffmpeg-buildmaster &&
-docker rm -f ffmpeg-buildmaster &&
-docker rmi -f ffmpeg-buildmaster saichaithanya/ffmpeg-build-toolchain:v1 &&
-python3 "$(pwd)"/tests/test.py
+	docker cp ffmpeg-buildmaster:/root/ffmpeg_bin/. "$bin_dir" &&
+	docker stop ffmpeg-buildmaster &&
+	docker rm -f ffmpeg-buildmaster &&
+	docker rmi -f ffmpeg-buildmaster saichaithanya/ffmpeg-build-toolchain:v1 &&
+	python3 "$(pwd)"/tests/test.py
